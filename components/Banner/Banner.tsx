64b996fba046e803d4a915b5e77c5c0efb4cd9a5
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-04-24 23:03:00 +0530

    banner added with profile photo

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,64 @@
+import styles from './Banner.module.scss';
+import Image from 'next/image';
+import profile from '../../public/sub.jpg';
+import { useEffect } from 'react';
+
+const Banner = () => {
+  useEffect(() => {
+    let text = document.getElementById('role');
+    if (text != undefined) {
+      text.innerHTML = text.innerText
+        .split('')
+        .map(
+          (char, i) =>
+            `<span style="transform:rotate(${i * 9.5}deg)">${char}</span>`
+        )
+        .join('');
+    }
+  }, []);
+  return (
+    <div className={styles['banner']}>
+      <div className={styles['profile']}>
+        <div className={styles['profile__pic']}>
+          {/* <Image
+          src={profile}
+          alt="profile"
+          //   width={400}
+          //   height={400}
+          layout="fixed"
+
+        /> */}
+          <div className={styles.name}>
+            <span>Mahammad Kasim</span>
+          </div>
+          <div className={styles.experience}>
+            <span className={styles['experience-year']}>4</span>
+            <span>Years Of Experience</span>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles['about']}>
+        <div className={styles.circle}></div>
+        <div className={styles.role} id="role">
+          Front-end Developer
+        </div>
+        <div className={styles['intro-container']}>
+          <span className={styles['tag--1']}>Hi, My name is</span>
+          <h1 className={styles.name}>Mahammad Kasim Nadim.</h1>
+          <h2 className={`${styles['tag--2']} text-background-clip`}>
+            I build things for the <span className={styles.web}>Web.</span>
+          </h2>
+          <div className={`${styles['tag--3']}`}>
+            I’m a Front-End Web developer based in Mangalore specializing in
+            building (and occasionally designing) exceptional digital
+            experiences. Love to Code.
+          </div>
+          {/* <button className={styles['text-me']}>Text Me</button> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Header from '../components/Header/Header';
 import styles from './Home.module.scss';
 import { CgShapeHalfCircle } from 'react-icons/cg';
+import Banner from '../components/Banner/Banner';
 
 const Home: NextPage = () => {
   return (
@@ -13,12 +14,10 @@ const Home: NextPage = () => {
           {/* <span className={styles.icon}>
             <CgShapeHalfCircle />
           </span> */}
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
+          <Banner />
         </main>
 
-        <footer className={styles.footer}>
+        {/* <footer className={styles.footer}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
@@ -34,7 +33,7 @@ const Home: NextPage = () => {
               />
             </span>
           </a>
-        </footer>
+        </footer> */}
       </div>
     </>
   );

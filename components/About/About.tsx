74986198054688c1c7eb5e6fed671826f245d629
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-09-20 11:57:29 +0530

    removed profile card

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,52 +1,40 @@
 import styles from './About.module.scss';
-import { motion } from 'framer-motion';
-import { appear } from '../../animations/animations';
-import { useEffect, useState } from 'react';
 import { FaHtml5, FaCss3Alt, FaAngular, FaReact } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io';
 import { SiTypescript, SiNextdotjs } from 'react-icons/si';
-import { isDesktop } from 'react-device-detect';
 
 const About = () => {
-  const [mouseEnter, setMouseEnter] = useState(false);
-  useEffect(() => {
-    const card = document.getElementById('card');
-    /* Get the height and width of the element */
-    const height = card?.clientHeight || 0;
-    const width = card?.clientWidth || 0;
+  const technologies = [
+    {
+      name: 'HTML5',
+      icon: <FaHtml5 />,
+    },
+    {
+      name: 'CSS3',
+      icon: <FaCss3Alt />,
+    },
+    {
+      name: 'Javascript',
+      icon: <IoLogoJavascript />,
+    },
+    {
+      name: 'Typescript',
+      icon: <SiTypescript />,
+    },
+    {
+      name: 'Angular',
+      icon: <FaAngular />,
+    },
+    {
+      name: 'ReactJS',
+      icon: <FaReact />,
+    },
 
-    function handleMouseMove(e: any) {
-      /* Store the x and y position */
-      const xVal = e.layerX;
-      const yVal = e.layerY;
-      /*
-       * Calculate rotation value along the Y-axis
-       * Here the multiplier 20 is to Control the rotation
-       */
-      const yRotation = 20 * ((xVal - width / 2) / width);
-      /* Calculate the rotation along the X-axis */
-      const xRotation = -20 * ((yVal - height / 2) / height);
-      /* Generate string for CSS transform property */
-      const transformCSS =
-        'perspective(500px) rotateX(' +
-        xRotation +
-        'deg) rotateY(' +
-        yRotation +
-        'deg)';
-
-      /* Apply the calculated transformation */
-      card!.style.transform = transformCSS;
-    }
-    if (isDesktop) {
-      card?.addEventListener('mousemove', handleMouseMove);
-      card?.addEventListener('mouseout', function () {
-        card!.style.transform = 'perspective(500px) rotateX(0) rotateY(0)';
-      });
-      card?.addEventListener('mouseenter', () => setMouseEnter(true));
-      card?.addEventListener('mouseleave', () => setMouseEnter(false));
-    }
-  }, []);
-
+    {
+      name: 'NextJS',
+      icon: <SiNextdotjs />,
+    },
+  ];
   return (
     <section id="about" className={styles.about}>
       <h2 className={styles.heading}>About Me</h2>
@@ -81,70 +69,17 @@ const About = () => {
               Here are a few technologies I&apos;ve been working with recently:
             </h3>
             <ul>
-              <li>
-                <span>
-                  <FaHtml5 className={styles.icon} />
-                  HTML5
-                </span>
-              </li>
-              <li>
-                <span>
-                  <FaCss3Alt className={styles.icon} />
-                  CSS3
-                </span>
-              </li>
-              <li>
-                <span>
-                  <IoLogoJavascript className={styles.icon} />
-                  Javascript
-                </span>
-              </li>
-              <li>
-                <span>
-                  <FaAngular className={styles.icon} />
-                  Angular
-                </span>
-              </li>
-              <li>
-                <span>
-                  <FaReact className={styles.icon} />
-                  ReactJS
-                </span>
-              </li>
-              <li>
-                <span>
-                  <SiTypescript className={styles.icon} />
-                  Typescript
-                </span>
-              </li>
-              <li>
-                <span>
-                  <SiNextdotjs className={styles.icon} />
-                  NextJS
-                </span>
-              </li>
+              {technologies.map((t) => (
+                <li key={t.name}>
+                  <span className="d-flex gap-2">
+                    {t.icon}
+                    {t.name}
+                  </span>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
-        <motion.div
-          variants={appear}
-          initial="initial"
-          animate="animate"
-          id="card"
-          className={`${styles.card} ${
-            mouseEnter ? styles['mouse-enter'] : styles['mouse-leave']
-          }`}
-        >
-          <div id="photo" className={styles.photo}>
-            <motion.div variants={appear} className={styles.name}>
-              <span>Mahammad Kasim</span>
-            </motion.div>
-            <motion.div variants={appear} className={styles.experience}>
-              <span className={styles['experience-year']}>4</span>
-              <span>Years Of Experience</span>
-            </motion.div>
-          </div>
-        </motion.div>
       </div>
     </section>
   );
